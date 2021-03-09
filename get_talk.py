da7b8c78c4fb89b6fdd2197bc3ddb571b96094d3
chengtianle1997 <chengtianle1997@163.com>
2021-03-09 15:17:39 +0800

    set api in gettalk class added

--- get_talk.py
+++ get_talk.py
@@ -9,41 +9,79 @@ import random
 import _thread
 
 class GetTalk:
-    def __init__(self, init_enable=False):
+    def __init__(self, init_enable=False, debugger=False):
         self.recorder = robot_recorder.robot_recorder()
         self.speaker = robot_speaker.robot_speaker()
         self.txt_to_voice = txt_to_voice.txt_to_voice()
         self.voice_to_txt = voice_to_txt.voice_to_txt()
         self.get_answer = get_answer.TuringRobot()
-
         # 录音状态指示
         self.recording = False
         # 初始化使能
         self.init_enable = init_enable
+        # 调试功能使能
+        self.debugger = debugger
         # 语音资源文件根目录
         self.audiodir = "resources/"
         # 检查文件配置
         if init_enable:
             self.InitAutoReply()
-
-
+        # 状态表
+        self.status = ["recording", "solving", "finished"]
     
     # 主功能函数：开始聊天
     def Talk(self):
+        _thread.start_new_thread(self.Talk_t)
+
+    def Talk_t(self):
         # 开始录音
+        self.set_status(self.status[0])
         self.recording = True
         self.recorder.record()
         self.recording = False
+        self.set_status(self.status[1])
         # 语音转文字
         code, txt, error_msg = self.voice_to_txt.convert()
-        # 错误解决
-                
+        self.set_question_txt(txt)
+        # 错误处理
+        if not code == 0:
+            if code == 888:
+                self.networkerr_sound()
+            elif code == 999:
+                self.internalerr_sound()
+            else:
+                self.internalerr_sound()
+            if self.debugger:
+                print("voice to txt error: code-{}, errmsg-{}".format(code, error_msg))
+            return
         # 问题转回答
         code, answer = self.get_answer.get_answer(txt)
+        self.set_answer_txt(answer)
+        # 错误处理
+        if not code == 0:
+            if code == 999:
+                self.networkerr_sound()
+            else:
+                self.internalerr_sound()
+            if self.debugger:
+                print("get answer error: code-{}".format(code))
+            return
         # 文字转语音
         code, error_msg = self.txt_to_voice.convert(answer)
+        # 错误处理
+        if not code == 0:
+            if code == 888:
+                self.networkerr_sound()
+            elif code == 999:
+                self.internalerr_sound()
+            else:
+                self.internalerr_sound()
+            if self.debugger:
+                print("txt to voice eror: code-{}, errmsg-{}".format(code, error_msg))
+            return
         # 播放语音
         self.speaker.play("answer.mp3")
+        self.set_status(self.status[2])
     
     # 主功能函数：手动停止录音（若录音过程未结束）
     def Stop(self):
@@ -98,14 +136,27 @@ class GetTalk:
         file_url = self.audiodir + "internalerr" + str(n) + ".mp3"
         _thread.start_new_thread(self.speaker.play, (file_url,))
 
+    # 设定问题文字
+    def set_question_txt(self, question_txt):
+        if self.debugger:
+            print(question_txt)
 
+    # 设定回答文字
+    def set_answer_txt(self, answer_txt):
+        if self.debugger:
+            print(answer_txt)
 
+    # 设定当前状态
+    def set_status(self, status):
+        if self.debugger:
+            print("status:{}".format(status))
 
 # Test Demo
 if __name__ == '__main__':
-    getTalk = GetTalk(init_enable=False)
-    getTalk.networkerr_sound()
-    getTalk.internalerr_sound()
-    # _thread.start_new_thread(getTalk.Talk)
-    time.sleep(6)
+    # 获取实例
+    getTalk = GetTalk(init_enable=False, debugger=True)
+    # 开始聊天
+    getTalk.Talk()
+    # 保持程序运行（主线程关闭，子线程随之关闭）
+    time.sleep(15)
     print("测试结束")
